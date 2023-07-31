6655ee40abcd77b6913dd5515b59100a67f08983
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2023-07-31 17:31:10 -0600

    change: background img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   return (
     <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
       {/* <Image src='/vercel.svg' alt='test background' width={100} height={24} priority /> */}
-      hi sports under construction
     </main>
   )
 }
