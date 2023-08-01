afd9929048b1246994eb8fa019b2f061351e86b7
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2023-08-01 13:28:07 -0600

    add: images endpoint config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sissamx.com.mx',
+        pathname: '/hi-sports/images/**'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
