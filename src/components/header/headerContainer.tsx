97181e95b01570b04b9d7cb4443f6f21577af07f
David Jimenez <david.jimenez@sissadigital.com>
2023-10-05 10:39:48 -0600

    Change: Se cambia el color del hamburguer a blanco

--- src/components/header/headerContainer.tsx
+++ src/components/header/headerContainer.tsx
@@ -6,19 +6,13 @@ import { Bebas_Neue } from 'next/font/google';
 import { Button, Drawer, theme } from 'antd';
 import ContentContainer from '../content/contentContainer';
 
-
-
 const bebas = Bebas_Neue({ weight: '400', preload: false });
 
 export default function HeaderContainer() {
     const [isMenuOpen, setIsMenuOpen] = React.useState(false);
 
     const menuItems = ['INICIO', 'PROGRAMACIÓN', 'ÚLTIMA HORA'];
-    const menuItemsHamburguer = [
-        "Breaking News", "Hi News", "Programación", "Contenido HS", "Podcast", "Próximos eventos", "Hi Punch",
-        "Pepsi Center", "Copa Francia", "Talento"
-
-    ]
+    const menuItemsHamburguer = ['Breaking News', 'Hi News', 'Programación', 'Contenido HS', 'Podcast', 'Próximos eventos', 'Hi Punch', 'Pepsi Center', 'Copa Francia', 'Talento'];
     const verMenu = (value: boolean) => {
         console.log('value', value);
         setIsMenuOpen(value);
@@ -35,30 +29,19 @@ export default function HeaderContainer() {
         setOpen(false);
     };
 
-
     return (
         <>
-
-            <Navbar maxWidth={"2xl"} className=' flex space-between fondoNav items-center' onMenuOpenChange={setIsMenuOpen}>
-
-                <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} className='sm:hidden' />
-
+            <Navbar maxWidth={'2xl'} className=' flex space-between fondoNav items-center' onMenuOpenChange={setIsMenuOpen}>
+                <NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} className='sm:hidden' style={{ color: 'white' }} />
 
                 <div className='md:flex items-center hidden'>
-                    <Button className='mi-boton' type="default" style={{ border: "none", width: "3rem", height: "3rem" }} onClick={showDrawer} icon={<MenuOutlined className='text-white icon-container' style={{ fontSize: "30px" }} />} />
+                    <Button className='mi-boton' type='default' style={{ border: 'none', width: '3rem', height: '3rem' }} onClick={showDrawer} icon={<MenuOutlined className='text-white icon-container' style={{ fontSize: '30px' }} />} />
                 </div>
 
                 <div className=' md:flex items-center hidden'>
                     <img className='w-20' src='https://sissamx.com.mx/hi-sports/images/logos/HI_SPORTS_LOGO.png' alt='logo' />
-                    <span className={`titleContent ${bebas.className} `}>
-                        #Todo es posible
-                    </span>
-
-
+                    <span className={`titleContent ${bebas.className} `}>#Todo es posible</span>
                 </div>
-
-
-
 
                 {/* 
                 <NavbarContent className='justify-between' justify='start'>
@@ -87,8 +70,6 @@ export default function HeaderContainer() {
                             ÚLTIMA HORA
                         </Link>
                     </NavbarItem>
-
-
                 </div>
 
                 <NavbarMenu className='fondoNav'>
@@ -102,36 +83,33 @@ export default function HeaderContainer() {
                     ))}
                 </NavbarMenu>
 
-
                 <div className='md:flex items-center hidden'>
-
                     <img className='w-10 mr-2' src='https://hi-sports.tv/media/Facebook.svg' alt='Follow us on Facebook' />
                     <img className='w-16' src='https://hi-sports.tv/media/Instagram.svg' alt='Follow us on Instagram' />
                     <img className='w-16' src='https://hi-sports.tv/media/twitter.svg' alt='Follow us on Twitter' />
-
                 </div>
-
             </Navbar>
 
             <div className='md:flex hidden  justify-between'>
-                <Drawer title={<div className='flex items-center'>
-                    <img className='w-20' src='https://sissamx.com.mx/hi-sports/images/logos/HI_SPORTS_LOGO.png' alt='logo' />
-                    <span className={`titleContent ${bebas.className} `}>
-                        #Todo es posible
-                    </span>
-
-
-                </div>
-                } style={{ background: "linear-gradient(45deg, #014aad, #692495, #692495, #014aad)", width: "350px" }} placement="left" onClose={onClose} open={open}>
+                <Drawer
+                    title={
+                        <div className='flex items-center'>
+                            <img className='w-20' src='https://sissamx.com.mx/hi-sports/images/logos/HI_SPORTS_LOGO.png' alt='logo' />
+                            <span className={`titleContent ${bebas.className} `}>#Todo es posible</span>
+                        </div>
+                    }
+                    style={{ background: 'linear-gradient(45deg, #014aad, #692495, #692495, #014aad)', width: '350px' }}
+                    placement='left'
+                    onClose={onClose}
+                    open={open}
+                >
                     {menuItemsHamburguer.map((item, index) => (
-                        <button key={index} style={{ color: "#ffee92" }} className={`flex justify-center text-xl mb-3 w-full py-3 rounded-md hover:bg-burger border-2 border-borderMorado`}>
-                            {item}</button>
+                        <button key={index} style={{ color: '#ffee92' }} className={`flex justify-center text-xl mb-3 w-full py-3 rounded-md hover:bg-burger border-2 border-borderMorado`}>
+                            {item}
+                        </button>
                     ))}
                 </Drawer>
-
             </div>
-
-
         </>
     );
 }
