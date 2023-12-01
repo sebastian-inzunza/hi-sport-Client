710d0c3b103c46678fdec3cedae8dd8b1a818eb0
Sebastian <sebastian_a9@hotmail.com>
2023-12-01 17:43:56 -0600

    feat: :art: show banned true and false, in videoblog

--- src/services/BlogsService.js
+++ src/services/BlogsService.js
@@ -1,95 +1,120 @@
 // services/apiService.js
-import axios from 'axios';
-
-
+import axios from "axios";
 
 export const getBlogs = async () => {
-  
-    try {
-      const response = await axios.get(`https://hi-sports-v1-5056641427c0.herokuapp.com/notes`)
-      
+  try {
+    const response = await axios.get(
+      `https://hi-sports-v1-5056641427c0.herokuapp.com/notes`
+    );
 
-      if (response.status === 200) {
-        return response.data.notes
-      } else {
-        return []
-        throw new Error(`La solicitud no fue exitosa. Código de estado: ${response.status}`);
-      }
-    } catch (error) {
-      console.error(error);
-      throw error; 
+    if (response.status === 200) {
+      const arregloDeObjetos = response.data.notes;
+
+      const objetosActivos = arregloDeObjetos.filter(
+        (objeto) => objeto.banned === true
+      );
+
+      return objetosActivos;
+    } else {
+      return [];
+      throw new Error(
+        `La solicitud no fue exitosa. Código de estado: ${response.status}`
+      );
     }
-  };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-  export const SearchSlug = async (slug) => {
+export const SearchSlug = async (slug) => {
+  try {
+    const response = await axios.get(
+      `https://hi-sports-v1-5056641427c0.herokuapp.com/notes/${slug}`
+    );
 
-    try {
-      const response = await axios.get(`https://hi-sports-v1-5056641427c0.herokuapp.com/notes/${slug}`)
-
-      if (response.status === 200) {
-        
-        return response.data
-      } else {
-        return []
-        throw new Error(`La solicitud no fue exitosa. Código de estado: ${response.status}`);
-      }
-    } catch (error) {
-      console.error(error);
-      throw error; // Propagar el error para que pueda ser manejado en el nivel superior
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      return [];
+      throw new Error(
+        `La solicitud no fue exitosa. Código de estado: ${response.status}`
+      );
     }
-  };
+  } catch (error) {
+    console.error(error);
+    throw error; // Propagar el error para que pueda ser manejado en el nivel superior
+  }
+};
 
+export const getVideoBlogs = async () => {
+  try {
+    const response = await axios.get(
+      `https://hi-sports-v1-5056641427c0.herokuapp.com/videoBlog`
+    );
 
-  export const getVideoBlogs = async () => {
-  
-    try {
-      const response = await axios.get(`https://hi-sports-v1-5056641427c0.herokuapp.com/videoBlog`)
-      
+    if (response.status === 200) {
+      const arregloDeObjetos = response.data.data;
 
-      if (response.status === 200) {
-        return response.data.data
-      } else {
-        return []
-        throw new Error(`La solicitud no fue exitosa. Código de estado: ${response.status}`);
-      }
-    } catch (error) {
-      console.error(error);
-      throw error; 
+      const objetosActivos = arregloDeObjetos.filter(
+        (objeto) => objeto.banned === true
+      );
+
+      return objetosActivos;
+    } else {
+      return [];
+      throw new Error(
+        `La solicitud no fue exitosa. Código de estado: ${response.status}`
+      );
     }
-  };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
-  export const SearchSlugVideo = async (slug) => {
+export const SearchSlugVideo = async (slug) => {
+  try {
+    const response = await axios.get(
+      `https://hi-sports-v1-5056641427c0.herokuapp.com/videoblog/${slug}`
+    );
 
-    try {
-      const response = await axios.get(`https://hi-sports-v1-5056641427c0.herokuapp.com/videoblog/${slug}`)
-
-      if (response.status === 200) {
-        
-        return response.data
-      } else {
-        return []
-        throw new Error(`La solicitud no fue exitosa. Código de estado: ${response.status}`);
-      }
-    } catch (error) {
-      console.error(error);
-      throw error; // Propagar el error para que pueda ser manejado en el nivel superior
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      return [];
+      throw new Error(
+        `La solicitud no fue exitosa. Código de estado: ${response.status}`
+      );
     }
-  };
+  } catch (error) {
+    console.error(error);
+    throw error; // Propagar el error para que pueda ser manejado en el nivel superior
+  }
+};
 
-  export const BlogReleated = async (id) => {
+export const BlogReleated = async (id) => {
+  try {
+    const response = await axios.get(
+      `https://hi-sports-v1-5056641427c0.herokuapp.com/notes/releated/${id}`
+    );
 
-    try {
-      const response = await axios.get(`https://hi-sports-v1-5056641427c0.herokuapp.com/notes/releated/${id}`)
+    if (response.status === 200) {
+      const arregloDeObjetos = response.data;
 
-      if (response.status === 200) {
-        
-        return response.data
-      } else {
-        return []
-        throw new Error(`La solicitud no fue exitosa. Código de estado: ${response.status}`);
-      }
-    } catch (error) {
-      console.error(error);
-      throw error; // Propagar el error para que pueda ser manejado en el nivel superior
+      const objetosActivos = arregloDeObjetos.filter(
+        (objeto) => objeto.banned === true
+      );
+
+      return objetosActivos
+    } else {
+      return [];
+      throw new Error(
+        `La solicitud no fue exitosa. Código de estado: ${response.status}`
+      );
     }
-  };
+  } catch (error) {
+    console.error(error);
+    throw error; // Propagar el error para que pueda ser manejado en el nivel superior
+  }
+};
--- src/services/publicidadService.js
+++ src/services/publicidadService.js
@@ -1,21 +1,29 @@
 import axios from "axios";
 
 export const getPublicidad = async () => {
-  
-    try {
-      const response = await axios.get(`https://hi-sports-v1-5056641427c0.herokuapp.com/publicity`)
-      
+  try {
+    const response = await axios.get(
+      `https://hi-sports-v1-5056641427c0.herokuapp.com/publicity`
+    );
 
-      if (response.status === 200) {
-        // return response.data.users; // Devolver los datos de la respuesta
-        return response.data.data
-      } else {
-        return []
-        throw new Error(`La solicitud no fue exitosa. Código de estado: ${response.status}`);
-      }
-    } catch (error) {
-      console.error(error);
-      throw error; 
+    if (response.status === 200) {
+      // return response.data.users; // Devolver los datos de la respuesta
+
+      const arregloDeObjetos = response.data.data;
+
+      const objetosActivos = arregloDeObjetos.filter(
+        (objeto) => objeto.banned === true
+      );
+
+      return objetosActivos;
+    } else {
+      return [];
+      throw new Error(
+        `La solicitud no fue exitosa. Código de estado: ${response.status}`
+      );
     }
-  };
-
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
