8b0b909956e17ace3a16e77c93aaec7504cfb794
Sebastian <sebastian_a9@hotmail.com>
2024-01-29 16:11:48 -0600

    feat:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 // App.js
+//@ts-nocheck
 
 import React from "react";
 import { ConfigProvider } from "antd";
@@ -6,7 +7,15 @@ import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
 import theme from "../components/themeConfig";
 import "../styles/globals.css";
+import NProgress from 'nprogress'; //nprogress module
+import Router from 'next/router';
 
+//Route Events. 
+
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ConfigProvider theme={theme}>
