c8ae2535d7872556ee850f67511bd8a1b6e79132
Sebastian <sebastian_a9@hotmail.com>
2024-02-06 13:50:31 -0600

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,20 +7,36 @@ import { NextUIProvider } from "@nextui-org/react";
 import type { AppProps } from "next/app";
 import theme from "../components/themeConfig";
 import "../styles/globals.css";
-import NProgress from 'nprogress'; //nprogress module
-import Router from 'next/router';
+import NProgress from "nprogress"; //nprogress module
+import Router from "next/router";
+import Head from 'next/head'; 
 
-//Route Events. 
+//Route Events.
 
-
-Router.events.on('routeChangeStart', () => NProgress.start()); 
-Router.events.on('routeChangeComplete', () => NProgress.done()); 
-Router.events.on('routeChangeError', () => NProgress.done());
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const App = ({ Component, pageProps }: AppProps) => (
   <ConfigProvider theme={theme}>
     <NextUIProvider>
-        <Component {...pageProps} />
+      <head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        ></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+              window.dataLayer.push(arguments);
+            }
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+          `}
+        </script>
+      </head>
+      <Component {...pageProps} />
     </NextUIProvider>
   </ConfigProvider>
 );
